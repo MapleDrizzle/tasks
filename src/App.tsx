882271743adca424ce9de3dab3aa423aebecdb62
Sophia Romero <siromero@udel.edu>
2024-02-07 12:23:40 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                <h2 className="name">Sophia Romero</h2>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
