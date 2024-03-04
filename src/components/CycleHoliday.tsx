fbf5c086d4cd402ac53f0c858972d28dfbf36c69
Sophia Romero <siromero@udel.edu>
2024-03-04 16:57:38 -0500

    Finished all functions but prettier error

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,43 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-
+// Holidays: Christmas, Thanksgiving, Valentine's Day, Halloween, New Year's Eve
+// 🎅 🦃 💝 🎃 🤡
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday = "🎅" | "🦃" | "💝" | "🎃" | "🤡";
+    const [holiday, setHoliday] = useState<Holiday>("🎅");
+
+    function advanceByAlph(): void {
+        setHoliday(
+            holiday === "🤡"
+                ? "🎅"
+                : holiday === "🎅"
+                ? "🎃"
+                : holiday === "🎃"
+                ? "🦃"
+                : holiday === "🦃"
+                ? "💝"
+                : "🤡"
+        );
+    }
+    function advanceByYear(): void {
+        setHoliday(
+            holiday === "💝"
+                ? "🤡"
+                : holiday === "🤡"
+                ? "🎃"
+                : holiday === "🎃"
+                ? "🦃"
+                : holiday === "🦃"
+                ? "🎅"
+                : "💝"
+        );
+    }
+
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>
+            <Button onClick={advanceByAlph}>Advance by Alphabet</Button>
+            <Button onClick={advanceByYear}>Advance by Year</Button>
+        </div>
+    );
 }
